2303ae00ccea3e07f985e7d6669e7c6dce30a061
Jay Huynh <jay.huynh@nib.com.au>
2023-03-05 22:27:00 +1000

    Disable mephisto git actions

--- examples/static_react_task/webapp/link_mephisto_task.sh
+++ examples/static_react_task/webapp/link_mephisto_task.sh
@@ -9,14 +9,10 @@ cd /mephisto
 yarn install
 yarn build-all
 
-# Link the mephisto-task package
-cd /mephisto/packages/mephisto-task
-npm link
-
 # Link the annotator-tracker package
-cd ../annotated/annotator-tracker
+cd /mephisto/packages/annotated/annotator-tracker
 npm link
 
 cd $current_dir
-npm link mephisto-task @annotated/annotator-tracker
+npm link @annotated/annotator-tracker
 npm install

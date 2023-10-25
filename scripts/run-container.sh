1a5a04f191a41840ed6a273f35ea709777c95b38
Jay Huynh <jay.huynh@nib.com.au>
2023-10-26 00:03:43 +1000

    Update serverless mephisto

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -7,16 +7,13 @@ M_TURK_PREVIEW_URL_PREFIX=${4:?"Specify 'M_TURK_PREVIEW_URL_PREFIX' as argv[4]"}
 AWS_ACCESS_KEY_ID=${5:?"Specify 'AWS_ACCESS_KEY_ID' as argv[5]"}
 AWS_SECRET_ACCESS_KEY=${6:?"Specify 'AWS_SECRET_ACCESS_KEY' as argv[6]"}
 
-
+LOG_TIME=$(date +%s)
 
 docker run -d --rm --init -v $APP_NAME-volume:/mephisto/data \
     --net mephistonginx_mephisto-net \
     --name $APP_NAME \
-    -e HEROKU_API_KEY=$HEROKU_API_KEY \
     -e APP_ENV=$APP_ENV \
     -e APP_NAME=$APP_NAME \
-    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     $APP_NAME;
 
 container_id=$(docker ps -q --filter ancestor=$APP_NAME --format="{{.ID}}");
@@ -31,17 +28,19 @@ fi
 
 mkdir -p ~/logs/$APP_NAME/;
 
-LOG_FILE="$HOME/logs/$APP_NAME/$container_id-$(date +%s).log";
+LOG_FILE="$HOME/logs/$APP_NAME/$container_id-$LOG_TIME.log";
 echo "Streaming logs from container $container_id to file $LOG_FILE";
 DOCKER_LOGS=$(docker inspect --format='{{.LogPath}}' $container_id);
 sudo ln "$DOCKER_LOGS" "$LOG_FILE";
 sudo chmod 777 "$LOG_FILE";
 
-echo "Waiting for MTurk preview URL: ";
+echo "Waiting for confirmation from providers: ";
+
 timeout 1800 \
-    sh -c "while ! docker logs -f $container_id| grep -q '$M_TURK_PREVIEW_URL_PREFIX'; \
+    sh -c "while ! grep '$M_TURK_PREVIEW_URL_PREFIX' '$LOG_FILE'; \
             do sleep 1; done";
 
 
-echo "MTurk preview URL: ";
+echo "Matched confirmation: ";
+grep $M_TURK_PREVIEW_URL_PREFIX $LOG_FILE;
 docker logs $container_id;

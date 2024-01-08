053a380bb45b68cb0f615f887e7f15639b16445e
Jay Huynh <jay.huynh@nib.com.au>
2024-01-09 02:53:04 +1000

    Update deployment

--- app/deploy.sh
+++ app/deploy.sh
@@ -2,3 +2,5 @@
 
 mephisto register "$MTURK_TYPE" name="$MTURK_NAME" access_key_id="$MTURK_ACCESS_KEY_ID" secret_access_key="$MTURK_SECRET_ACCESS_KEY"
 mephisto register prolific name=prolific api_key="$PROLIFIC_API_KEY"
+
+python3 deploy.py
--- app/pre_deployment_hook.py
+++ app/pre_deployment_hook.py
@@ -1,10 +1,2 @@
-import os
-
-APP_NAME = os.getenv('APP_NAME', 'temp')
-APP_ENV = os.getenv('APP_ENV', 'dev')
 def handle(*args):
     print("Running pre deployment hook")
-
-    if APP_NAME == 'test' or APP_ENV == 'prod':
-        print("Running on remote server. Executing init_setup.sh")
-        os.system(f"sh {os.path.dirname(os.path.realpath(__file__))}/init_setup.sh")

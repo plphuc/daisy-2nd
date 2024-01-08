f045b59453b9185fdd664e4304ab5de11766d152
Jay Huynh <jay.huynh@nib.com.au>
2024-01-08 21:30:01 +1000

    Test

---
+++ app/init_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mephisto register "$MTURK_TYPE" name="$MTURK_NAME" access_key_id="$MTURK_ACCESS_KEY_ID" secret_access_key="$MTURK_SECRET_ACCESS_KEY"
+mephisto register prolific name=prolific api_key="$PROLIFIC_API_KEY"
--- app/sync_s3.sh
+++ app/sync_s3.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 APP_NAME=${1:-temp-app}
 S3_BUCKET_NAME=${2:-s3://mephisto-data}
+LOG_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 mephisto_data_root="/mephisto/data/results"
 
-echo "[$(date)] Syncing $mephisto_data_root to $S3_BUCKET_NAME/data-v2/$APP_NAME!"
-aws s3 sync $mephisto_data_root $S3_BUCKET_NAME/data-v2/$APP_NAME
+FOLDER_NAME="$APP_NAME-$LOG_TIME"
+
+echo "[$(date)] Syncing $mephisto_data_root to $S3_BUCKET_NAME/data-v2/$FOLDER_NAME!"
+aws s3 sync $mephisto_data_root $S3_BUCKET_NAME/data-v2/$FOLDER_NAME --profile mpt
 
 echo "[$(date)] Sync complete!"

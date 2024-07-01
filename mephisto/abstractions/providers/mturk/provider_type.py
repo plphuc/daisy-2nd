c89135f0dc0f2dd52429e6acb3a4bd2f9298eb95
plmphuc <plm.phuc@tego.global>
2024-07-01 19:42:42 +0700

    PROVIDER_TYPE = "'mturk_sandbox'"

--- mephisto/abstractions/providers/mturk/provider_type.py
+++ mephisto/abstractions/providers/mturk/provider_type.py
@@ -4,4 +4,4 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-PROVIDER_TYPE = "mturk"
+PROVIDER_TYPE = "'mturk_sandbox'"
